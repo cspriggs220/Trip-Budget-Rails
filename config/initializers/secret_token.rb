12b3ffacaeb15b70717207faec2dbe38ce0a3c8f
Cody Spriggs <codyspriggs@me.com>
2013-06-11 12:27:36 -0500

    Initial README and new rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TripBudgetRails::Application.config.secret_token = '14aeef2073bc27475039e9390a4e5e7e07238fa6e03b5d9fe37537d85dfe7338cbebb551e1e147cdc0235ff2c1087cb288f3604412a845e7a323035532253915'

19fc2626bc619596280db1f8c934638d8eac3a75
Cody Spriggs <codyspriggs@me.com>
2013-06-17 14:35:41 -0500

    User changes their email and password

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -2,7 +2,7 @@ When(/^I go to the homepage$/) do
   visit "/"
 end
 
-When "I open the page" do
+When "I show me the page" do
   save_and_open_page
 end
 
@@ -12,6 +12,10 @@ end
 
 When(/^I (?:click|press) "(.*?)"$/) do |text|
   click_link_or_button text
+end
+
+When(/^I fill in "(.*?)" with "(.*?)"$/) do |field_named, value|
+  fill_in field_named, with: value
 end
 
 When(/^I fill in "(.*?)" for "(.*?)"$/) do |value, field_name|

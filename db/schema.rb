e676bd6aca766940d0636cba89bbc2cf6e6e1dfc
Cody Spriggs <codyspriggs@me.com>
2013-06-18 10:15:53 -0500

    Add user_id to Trips table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130612051535) do
+ActiveRecord::Schema.define(:version => 20130618045311) do
 
   create_table "trips", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|

f1f8d50ba1cdbd4d20c42b14bd01bb323bd07e62
Cody Spriggs <codyspriggs@me.com>
2013-06-26 10:22:04 -0500

    Secret Token ENV variable added to config

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TripBudgetRails::Application.config.secret_token = '14aeef2073bc27475039e9390a4e5e7e07238fa6e03b5d9fe37537d85dfe7338cbebb551e1e147cdc0235ff2c1087cb288f3604412a845e7a323035532253915'
+
+if Rails.env.development? or Rails.env.test?
+  TripBudgetRails::Application.config.secret_token = '14aeef2073bc27475039e9390a4e5e7e07238fa6e03b5d9fe37537d85dfe7338cbebb551e1e147cdc0235ff2c1087cb288f3604412a845e7a323035532253915'
+else
+  TripBudgetRails::Application.config.secret_token = ENV['SECRET_TOKEN']
+end
